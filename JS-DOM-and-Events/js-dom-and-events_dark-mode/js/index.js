215ea4dd2b7f141bffb78e0bd1222158ac2898c3
Johanna Hammerl <johanna_hammerl@t-online.de>
2025-06-12 12:38:58 +0200

    solution

--- JS-DOM-and-Events/js-dom-and-events_dark-mode/js/index.js
+++ JS-DOM-and-Events/js-dom-and-events_dark-mode/js/index.js
@@ -1,3 +1,14 @@
 console.clear();
 
 const bodyElement = document.querySelector('[data-js="body"]');
+
+const buttonDark = document.querySelector ('[data-js="dark-mode-button"]');
+buttonDark.addEventListener ("click", () => {bodyElement.classList.add("dark")});
+
+const buttonLight = document.querySelector ('[data-js="light-mode-button"]');
+buttonLight.addEventListener ("click", () => {bodyElement.classList.remove("dark")});
+
+const buttonToggle = document.querySelector ('[data-js="toggle-button"]')
+buttonToggle.addEventListener ("click", () => {bodyElement.classList.toggle("dark")});
+
+
