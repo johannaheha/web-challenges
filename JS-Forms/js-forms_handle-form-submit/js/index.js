6d64150df8ebde74b0f530ba6fabbcbb5603b648
Johanna Hammerl <johanna_hammerl@t-online.de>
2025-06-17 15:44:14 +0200

    solution Forms handle

--- JS-Forms/js-forms_handle-form-submit/js/index.js
+++ JS-Forms/js-forms_handle-form-submit/js/index.js
@@ -1,3 +1,23 @@
 console.clear();
 
 const form = document.querySelector('[data-js="form"]');
+
+form.addEventListener("submit", (event)=>{
+    event.preventDefault();
+
+    const formData = new FormData(event.target);
+    const data = Object.fromEntries(formData);
+
+    console.log(data);
+
+    const age = Number(data.age);
+    const badness = Number(data.badness);
+    const ageBadnessSum = age + badness;
+
+    console.log(`The age-badness-sum of "${data.firstName}" is "${ageBadnessSum}"`);
+
+//     event.target.removeEventListener();
+//     event.target.elements.firstName.focus();
+
+})
+
