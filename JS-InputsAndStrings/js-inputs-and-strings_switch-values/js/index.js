377a65a4a78b5c840c08b4f5b1d56647d1e9e4eb
Johanna Hammerl <johanna_hammerl@t-online.de>
2025-06-17 11:07:40 +0200

    Solution SwitchValues

--- JS-InputsAndStrings/js-inputs-and-strings_switch-values/js/index.js
+++ JS-InputsAndStrings/js-inputs-and-strings_switch-values/js/index.js
@@ -1,13 +1,24 @@
 console.clear();
 
 const firstInput = document.querySelector('[data-js="first-input"]');
+const secondInput = document.querySelector ('[data-js="second-input"]');
+
+const switchButton = document.querySelector('[data-js="button-switch"]');
 const uppercaseButton = document.querySelector('[data-js="button-uppercase"]');
 const lowercaseButton = document.querySelector('[data-js="button-lowercase"]');
 
 uppercaseButton.addEventListener("click", () => {
   firstInput.value = firstInput.value.toUpperCase();
+  secondInput.value = secondInput.value.toLowerCase();
 });
 
 lowercaseButton.addEventListener("click", () => {
   firstInput.value = firstInput.value.toLowerCase();
+   secondInput.value = secondInput.value.toUpperCase();
 });
+//Werte tauschen zw 1&2 input
+switchButton.addEventListener("click", () => {
+  const temp = firstInput.value; //temporäre Variable
+  firstInput.value = secondInput.value;
+  secondInput.value = temp;
+})
