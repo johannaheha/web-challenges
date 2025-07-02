31bea800ed73777c4f4924abf1e7166d92075c61
Johanna Hammerl <johanna_hammerl@t-online.de>
2025-07-02 12:00:36 +0200

    SolutionReactAttributes

--- React-Basics/react-basics_attributes/src/App.jsx
+++ React-Basics/react-basics_attributes/src/App.jsx
@@ -1,5 +1,21 @@
 import "./styles.css";
 
 export default function App() {
-  return <article>Replace me with an article</article>;
+  return Article();
+}
+
+function Article() {
+  return (
+    <article className="article">
+      <h2 className="article__title">Text of your choice</h2>
+      <form>
+        <label htmlFor="Eingabe">Eingabefeld</label>
+        <input type="text" id="Eingabe"></input>
+      </form>
+      <a
+        className="article__link"
+        href="https://de.wikipedia.org/wiki/React"
+      >Click me for more information</a>
+    </article>
+  );
 }
