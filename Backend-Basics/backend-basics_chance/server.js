f65fc35651fda7b24fb0414ec3cc3de68513e1d4
Johanna Hammerl <johanna_hammerl@t-online.de>
2025-08-11 13:01:20 +0200

    solution

--- Backend-Basics/backend-basics_chance/server.js
+++ Backend-Basics/backend-basics_chance/server.js
@@ -3,26 +3,28 @@ import { Chance } from "chance";
 
 const chance = new Chance();
 
-export const server = createServer((request, response) => {
-  const age = chance.age();
-  const name = chance.name();
-  const profession = chance.profession();
-
-  if (request.url === "/") {
-    response.end(
-      `Hello, my name is ${name} and I am ${age} years old. I am a ${profession}`
-    );
-    return;
-  }
-});
-
-// Hier wird ein Objekt wiedergegeben: 
-
 // export const server = createServer((request, response) => {
-// const person= {
-// name: chance.name();
-// age: chance.age();
-// profession : chance.profession()})}
+//   const age = chance.age();
+//   const name = chance.name();
+//   const profession = chance.profession();
 
-// response.setHeader("Content-Type", "application/json")
-// response.end(JSON.stringify.person))
+//   if (request.url === "/") {
+//     response.end(
+//       `Hello, my name is ${name} and I am ${age} years old. I am a ${profession}`
+//     );
+//     return;
+//   }
+// });
+
+// Hier wird ein Objekt wiedergegeben:
+
+export const server = createServer((request, response) => {
+  const person = {
+    name: chance.name(),
+    age: chance.age(),
+    profession: chance.profession(),
+  };
+
+  response.setHeader("Content-Type", "application/json");
+  response.end(JSON.stringify(person));
+});
